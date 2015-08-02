ae414d13bbb2483b5f3ec5eab1b4555b76f4ea87
Ray Fang <i@lazarusx.com>
2015-08-02 13:34:49 +0800

    Add solution to LowestCommonAncestorOfABinaryTree

---
+++ src/com/lazarusx/leetcode/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,28 @@
+package com.lazarusx.leetcode;
+
+/**
+ * Created by LazarusX on 15/8/2.
+ */
+public class LowestCommonAncestorOfABinaryTree {
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left != null) {
+            return left;
+        } else if (right != null) {
+            return right;
+        } else {
+            return null;
+        }
+    }
+}
